242d305acfbc063098d78840c642c0248d9f22e9
Aradhya DIxit <115568799+AradhyaDixit18@users.noreply.github.com>
2024-05-19 19:42:32 +0530

    Update Login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -8,7 +8,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:12312/api/login', { username, password });
+      const response = await axios.post('https://mern-task-management-eight.vercel.app/login', { username, password });
       localStorage.setItem('token', response.data.token);
       window.location.href = '/boards';
     } catch (error) {
